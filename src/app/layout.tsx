c95e62d129b8130a0eb537c0973af624496fe7d6
Aleix Rabassa <aleix.rabassa@icr-evolution.com>
2026-01-24 22:16:54 +0100

    feat: enhance layout and providers for improved functionality and user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ReactNode } from "react";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "TSender",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(props: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <Providers>
+          {props.children}
+        </Providers>
       </body>
     </html>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,13 +4,19 @@ import { ReactNode } from "react"
 import config from "@/rainbowKitConfig"
 import { WagmiProvider } from "wagmi"
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useState } from "react"
+import "@rainbow-me/rainbowkit/styles.css"
 
 export function Providers(props: {children: ReactNode}) {
+    const [queryClient] = useState(() => new QueryClient())
     return (
         <WagmiProvider config={config}>
-            <RainbowKitProvider>
-                {props.children}
-            </RainbowKitProvider>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>                    
+                    {props.children}
+                </RainbowKitProvider>
+            </QueryClientProvider>
         </WagmiProvider>
     )
 }
