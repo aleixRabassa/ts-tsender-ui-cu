4e27e3d25507938cb05d8991ce8baa01b13dfc64
Aleix Rabassa <aleix.rabassa@icr-evolution.com>
2026-04-17 16:25:32 +0200

    feat: add test for airdrop form visibility based on wallet connection status

--- test/playwright/basic.spec.ts
+++ test/playwright/basic.spec.ts
@@ -9,3 +9,19 @@ test('has title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle("TSender");
 });
+
+test("should show airdrop form when wallet is connected, otherwise, not", async ({ page, context, metamaskPage, extensionId }) => {
+  await page.goto('/');
+  await expect(page.getByText("Please connect your wallet...")).toBeVisible()
+
+  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId)
+  await page.getByTestId("rk-connect-wallet").click();
+
+  await page.getByTestId("rk-wallet-option-io.metamask").waitFor({
+    state: "visible",
+    timeout: 30000
+  });
+
+  await page.getByTestId("rk-wallet-option-io.metamask").click();
+  await metamask.connectToDapp();
+});
